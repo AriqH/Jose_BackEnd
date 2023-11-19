e16eb2790778c5241603575a2e79dab29e0f9606
AriqH <89585448+AriqH@users.noreply.github.com>
2023-11-19 22:56:18 +0800

    chat

--- controllers/chatController.js
+++ controllers/chatController.js
@@ -10,52 +10,40 @@ const allowedAnimalTypes = ["kucing", "anjing", "sapi", "babi", "kuda"];
 
 export const chatController = async (req, res) => {
     try {
-      const { prompt } = req.body;
+        const { prompt } = req.body;
 
-      const isAnimalTypeQuestion = allowedAnimalTypes.some(type => prompt.toLowerCase().includes(type));
+        const isAnimalTypeQuestion = allowedAnimalTypes.some(type => prompt.toLowerCase().includes(type));
 
-      if (!isAnimalTypeQuestion) {
-        res.send("Maaf, pertanyaan hanya boleh berhubungan dengan jenis hewan seperti kucing, anjing, sapi, babi, atau kuda.");
-        return;
-      }
+        if (!isAnimalTypeQuestion) {
+            res.send("Maaf, pertanyaan hanya boleh berhubungan dengan jenis hewan seperti kucing, anjing, sapi, babi, atau kuda.");
+            return;
+        }
 
-      const responsePromise = openai.createChatCompletion({
-        model: "gpt-3.5-turbo",
-        messages: [
-          {
-            role: "system",
-            content: "Kamu adalah dokter hewan, saya akan bertanya tentang jenis hewan, gejala hewan, dan deskripsi gejalanya. berikan hasil analisis, jangan bertanya lagi hanya sekali saja dan juga berikan tindakan yang harus dilakukan dan apakah perlu tindak lanjut oleh dokter.",
-          },
-          {
-            role: "user",
-            content: prompt,
-          },
-        ],
-      });
+        const response = await openai.createChatCompletion({
+            model: "gpt-3.5-turbo",
+            messages: [
+                {
+                    role: "system",
+                    content: "Kamu adalah dokter hewan, saya akan bertanya tentang jenis hewan, gejala hewan, dan deskripsi gejalanya. berikan hasil analisis, jangan bertanya lagi hanya sekali saja dan juga berikan tindakan yang harus dilakukan dan apakah perlu tindak lanjut oleh dokter.",
+                },
+                {
+                    role: "user",
+                    content: prompt,
+                },
+            ],
+        });
 
-      // Atur waktu maksimum eksekusi ke 10 detik
-      const timeoutPromise = new Promise((resolve, reject) => {
-        setTimeout(() => {
-          reject(new Error('Waktu eksekusi melebihi batas maksimum.'));
-        }, 10000); // 10 detik
-      });
+        if (
+            !response.data.choices ||
+            !response.data.choices[0] ||
+            !response.data.choices[0].message
+        ) {
+            res.send("Maaf, tidak dapat memberikan jawaban untuk pertanyaan ini.");
+            return;
+        }
 
-      // Menggunakan Promise.race untuk menyelesaikan permintaan tercepat
-      const response = await Promise.race([responsePromise, timeoutPromise]);
-
-      // Menangani hasil respons sesuai kebutuhan
-      if (
-        !response.data.choices ||
-        !response.data.choices[0] ||
-        !response.data.choices[0].message
-      ) {
-        res.send("Maaf, tidak dapat memberikan jawaban untuk pertanyaan ini.");
-        return;
-      }
-
-      res.send(response.data.choices[0].message);
+        res.send(response.data.choices[0].message);
     } catch (error) {
-      res.send(error.message);
+        res.send(error.message);
     }
-  };
-
+};
