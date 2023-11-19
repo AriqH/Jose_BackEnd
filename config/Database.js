377fda810d92ce8a7e7c1741a1a6ce2272a4946f
AriqH <89585448+AriqH@users.noreply.github.com>
2023-11-19 20:49:36 +0800

    push5

--- config/Database.js
+++ config/Database.js
@@ -2,7 +2,8 @@ import { Sequelize } from "sequelize";
 
 const db = new Sequelize('auth_vet', "root", '',{
     host: "localhost",
-    dialect: "mysql"
+    dialect: "mysql",
+    dialectModule : require("mysql2"),
 })
 
 export default db
