dc015d455c7878d2b37d55107dd5e7f5cbc580a0
AriqH <89585448+AriqH@users.noreply.github.com>
2023-11-19 21:03:29 +0800

    bcryptjs

--- controllers/Users.js
+++ controllers/Users.js
@@ -1,5 +1,5 @@
 import Users from "../models/UserModel.js";
-import bcrypt from "bcrypt";
+import bcrypt from "bcryptjs";
 import jwt from "jsonwebtoken";
 import { refreshToken } from "./RefreshToken.js";
 
