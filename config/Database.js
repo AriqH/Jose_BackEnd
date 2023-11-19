8e457c8312174f4b1110c42e455f05dd53bdae54
AriqH <89585448+AriqH@users.noreply.github.com>
2023-11-19 22:05:24 +0800

    mysql

--- config/Database.js
+++ config/Database.js
@@ -1,10 +1,11 @@
 import { Sequelize } from "sequelize";
+import mysql from "mysql2"
 
 const db = new Sequelize('auth_vet', "ariq", '123456',{
     host: "ngrok.panti.my.id",
     port: "18561",
     dialect: "mysql",
-    dialectModule : require("mysql2"),
+    dialectModule : mysql,
 })
 
 export default db
